b18e08bc493f890c3d37256e567066e6eb21aee0
Basit Yousuf <88964244+programindz@users.noreply.github.com>
2023-09-04 23:38:05 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,8 +1,5 @@
 from twilio.rest import Client
-from dotenv import load_dotenv
 import os 
-
-load_dotenv()
 
 account_sid = os.getenv("TWILIO_ACCOUNT_SID")
 auth_token = os.getenv("TWILIO_AUTH_TOKEN")
